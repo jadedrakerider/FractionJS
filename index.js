e83a73ab9d16e5534b5b904d13406c258e470dec
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-09-03 00:15:47 -0700

    updated cjs function name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const cjsTest = require('./tests/tst.Fraction.cjs')
+const CJStest = require('./tests/tst.Fraction.cjs')
 const testing = true;
 
 if (testing) {
-    cjsTest()
+    CJStest()
 }
 
 
